eae598553742efa9da34c2b16c1f3924fe267796
Дмитрий Файзулин <fazeekda@outlook.com>
2018-10-17 13:52:57 +0300

    Object Session Factory

---
+++ src/main/java/dom/MySessionFactory.java
@@ -0,0 +1,22 @@
+package dom;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class MySessionFactory {
+    private SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+        if (sessionFactory == null){
+            final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+            try {
+                sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            } catch (Exception e){
+                StandardServiceRegistryBuilder.destroy(registry);
+            }
+        }
+        return sessionFactory;
+    }
+}
